0e959b91180152ea9631f4146e29f29eaca06ff2
enterth3r4in <believe.the.rain@gmail.com>
2015-10-27 14:08:06 -0400

    Updated reading the input file and outputs to a new file

--- Chapter7FileIO/Chapter7FileIO/Chapter7FileIO.py
+++ Chapter7FileIO/Chapter7FileIO/Chapter7FileIO.py
@@ -1,9 +1,24 @@
-fileInput = open("employeeData.txt","r")
+﻿fileInput = open("employeeData.txt","r")
+fileOutput = open("updatedEmployeeData.txt","w")
 name = ""
 address = ""
+delimiter = ","
+newline = "\n"
 payRate = 0.0
-RAISE = 0.5
+RAISE = 2.00
 
 #Read records inside fileInput
 for record in fileInput:
-    print(record)
+    #print(record)
+    #print(record.strip())
+    newlineNoReturn = record.strip()
+    newRecord = newlineNoReturn.split(',')
+    name = newRecord[0]
+    address = newRecord[1]
+    payRate = float(newRecord[2])
+    payRate += RAISE
+    print(name, " Pay Rate Updated To: ", payRate)
+    recordOut = name + delimiter + address + delimiter + str(payRate) + newline
+    fileOutput.write(recordOut)
+
+fileInput.close()
